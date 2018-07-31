a8ac3a38f995c1a391c65e9c3fede24e01ba2976
Michel Spirlandeli <michelspirlandeli10@gmail.com>
2018-07-30 22:51:27 -0300

    create project

---
+++ src/controllers/authController.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const User = require('../model/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) =>{
+    const { email } = req.body;
+
+    try{
+        if(await User.findOne({ email }))
+        return res.status(400).send({ error: 'User already exist'});
+
+        const user = await User.create(req.body);
+
+        user.password = undefined;
+
+        return res.send({ user });
+    }catch(err){
+        return res.status(400).send({ error: 'Registration falied'});
+    }
+});
+
+module.exports = app => app.use('/auth', router);
